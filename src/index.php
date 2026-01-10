23e02e87ea4224bb121fed858576eb784dab0ead
Khoirul Umam <yura27293@gmail.com>
2026-01-10 17:16:11 +0700

    route index

--- src/index.php
+++ src/index.php
@@ -14,4 +14,14 @@ switch ($request) {
     case '/admin';
         require __DIR__ . $page . '/admin/menus/index.php';
         break;
+    case '/input';
+        require __DIR__ . $page . '/admin/menus/input_produk.php';
+    case '/edit';
+        require __DIR__ . $page . '/admin/menus/edit_produk.php';
+    case '/update';
+        require __DIR__ . $page . '/admin/menus/update_produk.php';
+    case '/simpan';
+        require __DIR__ . $page . '/admin/menus/simpan_produk.php';
+    case '/hapus';
+        require __DIR__ . $page . '/admin/menus/hapus_produk.php';
 }
