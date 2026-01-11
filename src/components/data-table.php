f0c8a5fb6c2d091954cba33f336d8f5fa8bf3527
indrafetch09 <harindrawahyu131@gmail.com>
2026-01-11 12:52:45 +0700

    minor update for previous commit

--- src/components/data-table.php
+++ src/components/data-table.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Joko Admin</title>
+  <title>Joko Admin | Dev</title>
   <!-- plugins:css -->
   <?php require_once __DIR__ . '/../config/paths.php'; ?>
   <link rel="stylesheet" href="<?php echo asset('vendors/mdi/css/materialdesignicons.min.css'); ?>">
